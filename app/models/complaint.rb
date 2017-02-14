b9e405142d0dce82e484b4a325cc77aad3ea7805
Ricardo Cuevas <rcuevasjr91@gmail.com>
2017-02-14 12:11:12 -0500

    complaint&defaultaredone

--- app/models/complaint.rb
+++ app/models/complaint.rb
@@ -5,8 +5,8 @@ class Complaint
                                 CIVIL DIVISION
                        CASE NO.
 
-    #{eviction_case.name}
-    d/b/a/ #{eviction_case.name}
+    #{eviction_case.legal_name}
+    d/b/a/ #{eviction_case.dba}
           Plaintiff
 
     VS.
@@ -19,14 +19,16 @@ class Complaint
 
           PLAINTIFFS COMPLAINT FOR TENANT EVICTION
 
-    Plaintiff, #{eviction_case.name} d/b/a #{eviction_case.name} brings action against Defendant(s) Italo Javier Delgado and all others in possession and alleges:
+    Plaintiff, #{eviction_case.legal_name} d/b/a #{eviction_case.dba} brings action against Defendant(s) Italo Javier
+    Delgado and all others in possession and alleges:
 
     1.  This is an action to evict a tenant from real property in Miami Dade County, Florida.
     2.  Plaintiff is the Landlord, and Defendant(s) is/are the tenant of the following described property in said county:
-                                    6790 NW 186st Apt #101  Miami,  Fl  33015
-    3.  Defendant has possession of the property under written agreement, copy attached or oral agreement, to pay rent of $1577.00  payable on the first of the month.
+                                #{eviction_case.address} Apt #{eviction_case.apt_no}  #{eviction_case.city_state_zip}
+    3.  Defendant has possession of the property under written agreement, copy attached or oral agreement, to pay rent of #{eviction_case.rent}  payable on the first of the month.
     4.  Defendant failed to pay total rent due on February 01, 2017.
-    5.  Plaintiff served Defendant with a notice on  to pay the rent or deliver possession but the Defendant(s) refuses to do either, a copy of which notice is attached hereto as Plaintiff Exhibit \"A\".
+    5.  Plaintiff served Defendant with a notice on  to pay the rent or deliver possession but the Defendant(s) refuses to do either, a
+      copy of which notice is attached hereto as Plaintiff Exhibit \"A\".
     6.  Said written agreement provides for Attorneys fees in an action for tenants default thereunder.
     7.  Defendant(s) is/are not in the military service of the United States of America.
 
@@ -35,27 +37,27 @@ class Complaint
     I HEREBY CERTIFY THAT A TRUE AND CORRECT COPY OF THE FOREGOING COMPLAINT WAS SERVED UPON THE DEFENDANT (S).
 
     The foregoing instrument was acknowledged             ____________________________________________
-    before me this February 13, 2017  by Briana Miranda                      .       #{eviction_case.name}- Authorized Property Manager who is personally known to me.                                             8730 NW 36th Avenue
+    before me this February 13, 2017  by #{eviction_case.prop_asst}.       #{eviction_case.prop_asst}- Authorized Property Manager who is personally known to me.                                             8730 NW 36th Avenue
                                                                                                                    Miami, FL  33147
                                                                                                                   (305) 558-0060
 
     NX_____________________________________
     Notary
 
-     Name: #{eviction_case.name}
-    Commission No.: #{eviction_case.name}
-    My Commission Expires: #{eviction_case.name}
-    .
+
+
+
+
     IN THE COUNTY COURT, IN AND FOR MIAMI DADE COUNTY, FLORIDA
                 CIVIL DIVISION
-                CASE NO.   #{eviction_case.name}
+                CASE NO.   #{eviction_case.case_number}
 
 
-                xxxxxx
-            d/b/a xxxxxx
+            #{eviction_case.legal_name}
+            d/b/a #{eviction_case.dba}
             Plaintiff,
             vs.
-            xxxxxxxx
+            #{eviction_case.name}
 
             and all others in possession
             Defendant/
@@ -66,29 +68,31 @@ class Complaint
             )
             COUNTY OF MIAMI DADE  )
 
-            BEFORE ME the undersigned authority personally appeared before me, Briana Miranda and duly sworn deposed on oath says:
+            BEFORE ME the undersigned authority personally appeared before me, #{eviction_case.prop_asst} and duly sworn deposed on oath says:
 
             Affiant is the duly authorized agent of Plaintiff in this cause and has personal knowledge of all facts asserted in this statement.
 
-            Defendant, xxxxxxxxx known by Affiant not to be in the military service of the United States or any governmental agency or branch subject to the provisions of the Soldiers and Sailors Civil Relief Act.
+            Defendant, #{eviction_case.name}known by Affiant not to be in the military service of the United States or any governmental agency or branch
+            subject to the provisions of the Soldiers and Sailors Civil Relief Act.
 
             FURTHER AFFIANT SAYETH NAUGHT
 
             _______________________________________
             Affiant Signature
 
-            Affiant Name: xxxxx
-            Address: 8730 NW 36th Avenue Miami, FL  33147
-            Telephone No. xxxxxx
+            Affiant Name: #{eviction_case.prop_asst}
+            Address: #{eviction_case.address}#{eviction_case.city_state_zip}
+            Telephone No. _______
 
-            The foregoing instrument was sworn to and acknowledged before me personally on February 13, 2017 by Briana Miranda  who  is personally known to me
+            The foregoing instrument was sworn to and acknowledged before me personally on #{Time.now}by Briana Miranda  who  is personally known to me
 
             NX____________________________________
             NOTARY PUBLIC-STATE OF FLORIDA
 
-            Name: xxxxx
-            Commission No.:xxxxx
-            My Commission Expires xxxxxx
+
+
+
+
 
 
 
@@ -99,39 +103,37 @@ class Complaint
             IN THE COUNTY COURT IN AND FOR MIAMI DADE COUNTY, FLORIDA
 
                                   CIVIL DIVISION
-                        CASE NO:  xxxxxx
+                        CASE NO:  #{eviction_case.case_number}
                         JUDGE:
 
-            xxxxxx
-            d/b/a xxxxxx
+            #{eviction_case.legal_name}
+            d/b/a #{eviction_case.dba}
 
             vs.
 
-            xxxxxx
+            #{eviction_case.name}
 
             and all others in possession
 
             AFFIDAVIT DESIGNATING AUTHORITY TO PROPERTY MANAGERS
 
 
-            BEFORE ME, the undersigned authority personally appeared xxxxxx, Manager and Registered Agent on record for xxxxxx d/b/a xxxxxxxx who after being duly sworn deposes and says,
+            BEFORE ME, the undersigned authority personally appeared #{eviction_case.prop_asst}, Manager and Registered Agent on record for #{eviction_case.legal_name} d/b/a #{eviction_case.dba} who after being duly sworn deposes and says,
 
-            That she has given and granted xxxxxx  and xxxxxx authority to act as Authorized Property Managers on the behalf of xxxxxx d/b/a xxxxxxx to execute three day notices and seven day
+            That she has given and granted #{eviction_case.three_day_sign}  and #{eviction_case.prop_asst} authority to act as Authorized Property Managers on the behalf of #{eviction_case.legal_name} d/b/a #{eviction_case.dba} to execute three day notices and seven day
             notices to terminate, to sign and file pleadings for evictions, i.e., complaints for eviction.  Also, to sign and file motions for default, to obtain final judgments, writs of possession
-            for residential evictions in uncontested cases for xxxxx  d/b/a xxxxxx.
+            for residential evictions in uncontested cases for #{eviction_case.legal_name} d/b/a #{eviction_case.dba}.
 
             In addition, authority is hereby granted to said authorized property managers to attend Landlord/Tenant Mediations and/or Final Hearings and make decisions on behalf of
-            xxxxxxxx d/b/a xxxxxxx.
+            #{eviction_case.legal_name} d/b/a #{eviction_case.dba}.
 
 
 
-            The foregoing instrument was acknowledged before me on xxxxxxx , by xxxxxx, who is personally known to me.
+            The foregoing instrument was acknowledged before me on #{Time.now} , by #{eviction_case.prop_asst}, who is personally known to me.
 
 
                       NX________________________________________
                        Notary Public
-                                                                                                      Name: xxxxxx
-                             Commission No.: xxxxxx
-                                       My Commission Expires: xxxxx "
+                                                                                                      "
   end
 end
